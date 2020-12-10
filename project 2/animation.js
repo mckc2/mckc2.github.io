85389a3b95ac4de791b8c659bcddb120fdb47358
mckc2 <mckc2@pdx.edu>
2020-12-09 22:07:12 -1000

    new interaction

---
+++ project 2/animation.js
@@ -0,0 +1,9 @@
+<script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+    var controller = new Scrollmagic.Controller();
+    var scene = new Scrollmagic.Scene({
+    triggerElement '.container-1'
+     })
+    .setClassToggle('.container-1', 'show')
+    .addTo(controller);
+</script>
